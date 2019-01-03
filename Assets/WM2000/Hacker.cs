dea383945c50ec8baa2d9f7253b34287e5ed7361
tale97 <ttle@college.harvard.edu>
2019-01-02 20:31:06 -0500

    1st commit terminal hacker excluding Library folder

---
+++ Assets/WM2000/Hacker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Game state
+    int level;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu("Hello Tuan,");
+    }
+
+    void ShowMainMenu(string greeting)
+    {
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("What would you like to hack into?\n");
+        Terminal.WriteLine("Press 1 for the local library");
+        Terminal.WriteLine("Press 2 for the police station");
+        Terminal.WriteLine("Press 3 for the Pentagon\n");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu("Welcome back, Tuan");
+        } // TODO handle differently depending on screen
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+ 
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose level 1, 2, 3, or menu");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
